29f97feb49687dd40d9ef1a11e070bde40349627
Im_Cynx <silentninj4.7@gmail.com>
2022-01-30 20:29:23 +0100

    + Moving objects now have weight in center screen

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -8,18 +8,20 @@ export default function Cursor() {
     document.onmousemove = (event) => {
         let docs = (document.querySelectorAll(".custom-cursor") as NodeListOf<HTMLElement>);
         let game = (document.querySelectorAll(".button-inner-section, .red-button-section, .guess-int") as NodeListOf<HTMLElement>);
+        let height = window.screen.availHeight;
+        let width = window.screen.availWidth;
 
         docs.forEach((x) => {
             x.style.transform = "translate(calc(" + event.pageX + "px - 5rem), calc(" + event.pageY + "px - 5rem))";
         });
 
         game.forEach((x) => {
-            x.style.transform = "translate( " +  (event.pageX / 150) + "px, " + (event.pageY / 150) + "px)";
+            x.style.transform = "translate( " +  ((event.pageX - width/2) / 150) + "px, " + ((event.pageY - height/2) / 150) + "px)";
         });
 
         [1,2,3,4,5,6].forEach((x) => {
-            (document.querySelector(".start-bubble-" + x) as HTMLElement).style.transform = "translate(" + (event.pageX / 50) * (x % 3 === 1 ? 0.5 : ((x - 1) % 3) / 1.5) + 
-                                                                                            "px, " + (event.pageY / 50) * (x % 3 === 1 ? 0.5 : ((x - 1) % 3) / 1.5) + "px)";
+            (document.querySelector(".start-bubble-" + x) as HTMLElement).style.transform = "translate(" + ((event.pageX - width/2) / 50) * (x % 3 === 1 ? 0.5 : ((x - 1) % 3) / 1.5) + 
+                                                                                            "px, " + ((event.pageY - height/2) / 50) * (x % 3 === 1 ? 0.5 : ((x - 1) % 3) / 1.5) + "px)";
         });
     } 
 
