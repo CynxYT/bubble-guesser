bb4b032a9cde807f30db860ba00cd1c4e4507eb6
LAPTOP-RCLUCR94\silen <t.koree@student.utwente.nl>
2022-04-23 12:39:11 +0200

    + Add socials to hover items

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,21 +1,18 @@
-import React from "react";
-
-
 
 export default function Socials() {
 
     return (
         <div className="socials-tab">
-            <a className="social-container" href="https://www.twitter.com/im_cynx" rel="noreferrer" target="_blank">
+            <a className="social-container hover-item" href="https://www.twitter.com/im_cynx" rel="noreferrer" target="_blank">
                 <div className="twitter-icon"/>
             </a>
-            <a className="social-container" href="https://www.instagram.com/im_cynx" rel="noreferrer" target="_blank">
+            <a className="social-container hover-item" href="https://www.instagram.com/im_cynx" rel="noreferrer" target="_blank">
                 <div className="instagram-icon"/>
             </a>
-            <a className="social-container" href="https://www.linkedin.com/in/cynx" rel="noreferrer" target="_blank">
+            <a className="social-container hover-item" href="https://www.linkedin.com/in/cynx" rel="noreferrer" target="_blank">
                 <div className="linkedin-icon"/>
             </a>
-            <a className="social-container" href="https://programming.cynx-productions.com" rel="noreferrer" target="_blank">
+            <a className="social-container hover-item" href="https://programming.cynx-productions.com" rel="noreferrer" target="_blank">
                 <div className="watermark-icon"/>
             </a>
         </div>
